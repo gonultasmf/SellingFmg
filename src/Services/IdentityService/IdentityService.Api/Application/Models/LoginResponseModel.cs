58bfc8629dbd09ed7f51b23cea7e21741311219a
Mustafa Gönültaş <gonultasmf@hotmail.com>
2023-07-09 18:58:06 +0300

    Identity Service completed.

---
+++ src/Services/IdentityService/IdentityService.Api/Application/Models/LoginResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityService.Api;
+
+public class LoginResponseModel
+{
+    public string UserName { get; set; }
+    public string UserToken { get; set; }
+}
---
+++ src/Services/IdentityService/IdentityService.Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IIdentityService _identityService;
+
+    public AuthController(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Login([FromBody] LoginRequestModel model)
+    {
+        var result = await _identityService.Login(model);
+
+        return Ok(result);
+    }
+}
