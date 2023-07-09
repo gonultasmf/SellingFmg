a35d2d09dd89e8a37389623f9ad2b1ce68342e43
Mustafa Gönültaş <gonultasmf@hotmail.com>
2023-07-09 16:48:50 +0300

    catalog servise completed.

---
+++ src/Services/CatalogService/CatalogService.Api/Program.cs
@@ -0,0 +1,43 @@
+using CatalogService.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureDbContext();
+builder.Services.Configure<CatalogSettings>(configuration.GetSection("CatalogSettings"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MigrateDbContext<CatalogContext>((context, services) =>
+{
+    var env = services.GetService<IWebHostEnvironment>();
+    var logger = services.GetService<ILogger<CatalogContextSeed>>();
+
+    new CatalogContextSeed().SeedAsync(context, logger).Wait();
+
+});
+
+app.Run();
---
+++ src/Services/CatalogService/CatalogService.Infrastructure/Context/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using CatalogService.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Infrastructure;
+
+public class CatalogContext : DbContext
+{
+    public const string DEFAULT_SCHEMA = "catalog";
+
+    public CatalogContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<CatalogItem> CatalogItems { get; set; }
+    public DbSet<CatalogBrand> CatalogBrands { get; set; }
+    public DbSet<CatalogType> CatalogTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CatalogBrandEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new CatalogItemEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new CatalogTypeEntityTypeConfiguration());
+    }
+}
