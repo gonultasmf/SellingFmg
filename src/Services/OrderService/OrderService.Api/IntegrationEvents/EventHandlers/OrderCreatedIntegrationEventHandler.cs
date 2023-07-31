eca2d15c7fff1d77fc68bde4e7b1dfd14522c297
Mustafa Gönültaş <gonultasmf@hotmail.com>
2023-07-31 20:33:07 +0300

    geliştirme yapıldı api için

---
+++ src/Services/OrderService/OrderService.Api/IntegrationEvents/EventHandlers/OrderCreatedIntegrationEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using EventBus.Base.Abstraction;
+using MediatR;
+using OrderService.Application;
+
+namespace OrderService.Api;
+
+public class OrderCreatedIntegrationEventHandler : IIntegrationEventHandler<OrderCreatedIntegrationEvent>
+{
+    private readonly IMediator mediator;
+    private readonly ILogger<OrderCreatedIntegrationEventHandler> logger;
+
+    public OrderCreatedIntegrationEventHandler(IMediator mediator, ILogger<OrderCreatedIntegrationEventHandler> logger)
+    {
+        this.mediator = mediator;
+        this.logger = logger;
+    }
+
+    public async Task Handle(OrderCreatedIntegrationEvent @event)
+    {
+        logger.LogInformation("Handling integration event: {IntegrationEventId} at {AppName} - ({@IntegrationEvent})", 
+            @event.Id, typeof(OrderCreatedIntegrationEventHandler).Namespace, @event);
+
+        var createOrderCommand = new CreateOrderCommand(@event.Basket.Items, @event.UserName,
+            @event.City, @event.Street, @event.State, @event.Country, @event.ZipCode, @event.CardNumber,
+            @event.CardHolderName, @event.CardExpiration, @event.CardSecurityNumber, @event.CardTypeId);
+
+        await mediator.Send(createOrderCommand);
+    }
+}
